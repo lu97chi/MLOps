9d021ed6e157f95a459d347528e3665d9b46e5af
lu97chi <lu97is@gmail.com>
2024-09-15 22:36:46 -0600

    updated credentials

--- azureml_config.py
+++ azureml_config.py
@@ -5,7 +5,7 @@ from azure.ai.ml import MLClient
 credential = ClientSecretCredential(
     tenant_id=os.getenv('AZURE_TENANT_ID'),
     client_id=os.getenv('AZURE_CLIENT_ID'),
-    client_secret=os.getenv('AZURE_CLIENT_SECRET')
+    client_secret=os.getenv('AZURECLIENTSECRET')
 )
 
 ml_client = MLClient(

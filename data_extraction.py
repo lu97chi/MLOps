93f4327bb9f5b2cf7d844e837894e9fbc594c329
lu97chi <lu97is@gmail.com>
2024-09-15 18:59:47 -0600

    updated snow connection

--- data_extraction.py
+++ data_extraction.py
@@ -9,8 +9,13 @@ conn = snowflake.connector.connect(
     account=os.environ.get('SNOWFLAKEACCOUNT'),
     warehouse=os.environ.get('SNOWFLAKEWAREHOUSE'),
     database=os.environ.get('SNOWFLAKEDATABASE'),
-    schema=os.environ.get('SNOWFLAKESCHEMA')
+    schema=os.environ.get('SNOWFLAKESCHEMA'),
+    role=os.environ.get('SNOWFLAKEROLE'),
+    login_timeout=120,
+    network_timeout=120,
+    ocsp_fail_open=True
 )
+
 
 # Execute query
 query = 'SELECT * FROM WINE;'
@@ -21,3 +26,4 @@ df.to_csv('data.csv', index=False)
 
 # Close connection
 conn.close()
+
